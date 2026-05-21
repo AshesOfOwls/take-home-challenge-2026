a2daab1b5e16e00fbc6afa5db594171ae3be33c2
Quinn Elwood <ashesofowls@gmail.com>
2026-05-21 16:32:02 -0700

    T-01: wire useQuery(fetchProducts) into the storefront

--- src/your-storefront.tsx
+++ src/your-storefront.tsx
@@ -1,16 +1,22 @@
-import { PageWrapper } from './components/layout/page-wrapper';
+import { useQuery } from '@tanstack/react-query';
 import styled from '@emotion/styled';
-import { ProductCard } from './components/product-card';
 
-const API_URL = 'http://localhost:1337/get-products';
+import { PageWrapper } from './components/layout/page-wrapper';
+import { ProductCard } from './components/product-card';
+import { fetchProducts } from './api/products';
 
 export function YourStorefront() {
-  // fetch the data via useQuery from @tanstack/react-query!
+  const { data: products } = useQuery({
+    queryKey: ['products'],
+    queryFn: fetchProducts,
+  });
 
   return (
     <PageWrapper heading='Your Storefront' icon='menu'>
       <ProductGrid>
-        <ProductCard />
+        {(products ?? []).map((product) => (
+          <ProductCard key={product.id} />
+        ))}
       </ProductGrid>
     </PageWrapper>
   );
