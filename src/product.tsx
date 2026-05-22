00d025aecee31253dc291fc77df0b1147ef7bce4
Quinn Elwood <ashesofowls@gmail.com>
2026-05-21 17:50:02 -0700

    T-14: replace PDP placeholder with image + detail-slot scaffold

--- src/product.tsx
+++ src/product.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { useParams } from 'react-router-dom';
 
+import media from 'utils/media-queries';
 import { useProduct } from './hooks/use-product';
 
 export function ProductPage() {
@@ -9,48 +10,42 @@ export function ProductPage() {
 
   return (
     <Container>
-      <Header>{product?.name}</Header>
-      <Divider />
-      <Description>
-        This is a placeholder product detail page. It will contain the description, allotment details, and other product
-        information.
-      </Description>
-      <ImageContainer>
-        <img
-          src={
-            'https://images.dutchie.com/519e2ac68c3968e4b20d9910e3b473aa?auto=format&dpr=2&bg=FFFFFF&crop=faces&fit=crop&max-h=291&max-w=409&min-h=291&min-w=409&ixlib=react-7.2.0'
-          }
-        />
-      </ImageContainer>
+      <ImageSection />
+      <DetailsSection>
+        <Heading>{product?.name}</Heading>
+        <Pricing />
+        <Description />
+        <Metadata />
+      </DetailsSection>
     </Container>
   );
 }
 
 const Container = styled.div`
-  padding: 50px;
-  width: 100%;
-`;
-
-const Header = styled.h1`
-  font-size: 20px;
-  font-weight: 700;
-  text-align: center;
-  margin: 0;
-`;
-
-const Divider = styled.div`
-  width: 50%;
-  height: 1px;
-  background-color: #c8ced4;
-  margin: 20px auto;
-`;
-
-const Description = styled.p`
-  text-align: center;
-  padding: 1px;
-`;
-
-const ImageContainer = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
+
+  ${media.tablet`
+    flex-direction: row;
+  `}
 `;
+
+const ImageSection = styled.div`
+  flex: 1;
+`;
+
+const DetailsSection = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Heading = styled.div``;
+
+const Pricing = styled.div``;
+
+const Description = styled.div``;
+
+const Metadata = styled.div``;
